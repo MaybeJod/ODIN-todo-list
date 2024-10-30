435e478cea63d254cf2ef66440a0c57f5f193fd1
MaybeJod <135610495+MaybeJod@users.noreply.github.com>
2024-10-30 22:07:35 +0100

    added aside with grid

--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
 import "./styles/reset.css";
 import "./styles/style.css";
 
-console.log("hello")
+//add task button
+    //creates what a task needs
+
+//task needs: 
+    //title - text
+    //notes - text
+    //deadline - date
+    //priority - dropdown with options
+    //what projects / main task DB - dropdown with options
+    
+//store tasks
+//display tasks
+console.log('hello')
+console.log('world')
+
+
